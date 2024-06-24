df3be7083645458a725fa8865db130457a23ebbe
yakuphantonintshi <yakupha58756@gmail.com>
2024-06-24 08:42:05 +0200

    Add_Products

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <Friends />
+    </div>
+    <div class="row">
+      <Products />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Friends from '@/components/Friends.vue'
+import Products from '@/components/Products.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Friends,
+    Products
   }
 }
 </script>
